613f407252fce86ea0daacd21e1f8c040deedf36
Gary Fredericks <fredericksgary@gmail.com>
2021-05-17 14:18:50 -0500

    Bump dep version

--- lein-how-to-ns/project.clj
+++ lein-how-to-ns/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :vcs :git
-  :dependencies [[com.gfredericks/how-to-ns "0.2.8"]]
+  :dependencies [[com.gfredericks/how-to-ns "0.2.9"]]
   :deploy-repositories [["releases" :clojars]])
